78719b314e03d09eda3b4285fe798953d8e2d70c
Pedro Chagas <87217922+pChagas-cloud@users.noreply.github.com>
2021-10-05 13:44:44 -0300

    versão final

--- c++/iniciante/jogo_adivinhação.cpp
+++ c++/iniciante/jogo_adivinhação.cpp
@@ -28,16 +28,25 @@ std::cout << "**************************************" << std::endl;
 bool jogando = true;
 int rodada = 1;
 const int NUMERO_SECRETO = gen_random();
+int pontos = 100;
+int taxa_erro = 20;
 
 
 while (jogando == true){
+
+
+    if(pontos == 0 ){
+        std::cout << "Você perdeu todos os pontos... tente novamente!" << std::endl;
+        jogando = false;
+        break;
+    }
 
     std::cout << rodada << " rodada" << std::endl;
     string chute_str;
 
     if (rodada < 2){
         std::cout << "Digite seu chute: ";
-        std::cin >> chute_str;  
+        std::cin >> chute_str;
     }
     else{
         std::cout << "Digite seu próximo chute: ";
@@ -46,7 +55,7 @@ while (jogando == true){
 
     bool check_input = isNumeric(chute_str);
 
-    if (check_input == true){
+    if (check_input == true && pontos > 0){
 
         rodada++;
         int chute = stoi(chute_str);
@@ -57,18 +66,27 @@ while (jogando == true){
 
 
         if(menor_que)
-        {std::cout << "O seu chute: " << chute << " foi menor do que o número secreto" << std::endl;}       
+        {
+          std::cout << std::endl; 
+          std::cout << "O seu chute: " << chute << " foi menor do que o número secreto, agora você tem " << pontos-taxa_erro << " pontos" << std::endl; 
+          pontos = pontos - taxa_erro; 
+          std::cout << "======================================"<< std::endl; 
+          std::cout << std::endl;}
 
         else if(maior_que)
-        {std::cout << "O seu chute: " << chute << " foi maior do que o número secreto" << std::endl;}
+        {
+         std::cout << std::endl; 
+         std::cout << "O seu chute: " << chute << " foi maior do que o número secreto, agora você tem " << pontos-20 << " pontos" <<  std::endl; 
+         pontos = pontos - taxa_erro; 
+         std::cout << "======================================"<< std::endl; 
+         std::cout << std::endl;}
 
-        else if (acertou) {std::cout << "Parabéns! você acertou !!! " << std::endl; jogando = false;}
+        else if (acertou) {std::cout << "Parabéns! você acertou e ganhou com " << pontos << " pontos!!!" << std::endl; jogando = false;}
 
     }
-
-    else{
+  
+    else if(check_input == false && pontos > 0){
         std::cout << "Chute um número !" << std::endl;
     }
-
 }
 }
